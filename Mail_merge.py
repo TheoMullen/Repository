721cc4d64c00441284b5bc2f110779daa8254b48
TheoMullen <101186063+TheoMullen@users.noreply.github.com>
2022-04-28 14:32:27 +0100

    Update Mail_merge.py

--- Mail_merge.py
+++ Mail_merge.py
@@ -1,20 +1,11 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
+invited_names = "./Mail Merge Project Start/Input/Names/invited_names.txt"
 
+send_folder = "./MailMergeProjectStart/Output/ReadyToSend"
 
-invited_names = "/Users/theo/Downloads/Mail Merge Project Start/Input/Names/invited_names.txt"
+starting_letter = "./Mail Merge Project Start/Input/Letters/starting_letter.txt"
 
-send_folder = "/Users/theo/Downloads/MailMergeProjectStart/Output/ReadyToSend"
-
-starting_letter = "/Users/theo/Downloads/Mail Merge Project Start/Input/Letters/starting_letter.txt"
 
 
 
